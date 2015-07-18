3db3705c978ed58c5a629d8e29367d58c37c0911
Alex Gaynor <alex.gaynor@gmail.com>
2015-07-18 09:42:21 -0400

    refactor to always include the tag

--- src/lib.rs
+++ src/lib.rs
@@ -17,13 +17,13 @@ impl<W> Serializer<W> where W: io::Write {
         }
     }
 
-    fn _write_length(&mut self, length: isize) -> Result<(), io::Error> {
+    fn _write_length(&mut self, length: usize) -> Result<(), io::Error> {
         assert!(length < 128);
         try!(self.writer.write_u8(length as u8));
         return Ok(());
     }
 
-    fn _int_length(&self, v: i64) -> isize {
+    fn _int_length(&self, v: i64) -> usize {
         let mut num_bytes = 1;
         let mut i = v;
 
@@ -33,30 +33,40 @@ impl<W> Serializer<W> where W: io::Write {
         }
         return num_bytes;
     }
+
+    fn _write_with_tag<F>(&mut self, tag: u8, body: F) -> Result<(), io::Error>
+            where F: Fn() -> Vec<u8> {
+        try!(self.writer.write_u8(tag));
+
+        let body = body();
+        try!(self._write_length(body.len()));
+        try!(self.writer.write_all(&body));
+        return Ok(());
+    }
 }
 
 impl<W> serde::Serializer for Serializer<W> where W: io::Write {
     type Error = io::Error;
 
     fn visit_bool(&mut self, v: bool) -> Result<(), Self::Error> {
-        if v {
-            try!(self.writer.write_all(b"\xff"));
-        } else {
-            try!(self.writer.write_all(b"\x00"));
-        }
-        return Ok(());
+        return self._write_with_tag(1, || {
+            if v {
+                return b"\xff".to_vec();
+            } else {
+                return b"\x00".to_vec();
+            }
+        })
     }
 
     fn visit_i64(&mut self, v: i64) -> Result<(), Self::Error> {
-        try!(self.writer.write_all(b"\x02"));
-
         let n = self._int_length(v);
-        try!(self._write_length(n));
-        for i in (1..n+1).rev() {
-            try!(self.writer.write_u8((v >> ((i - 1) * 8)) as u8))
-        }
-
-        return Ok(());
+        return self._write_with_tag(2, || {
+            let mut result = Vec::with_capacity(n);
+            for i in (1..n+1).rev() {
+                result.push((v >> ((i - 1) * 8)) as u8);
+            }
+            return result;
+        });
     }
 
     #[allow(unused_variables)]
@@ -144,8 +154,8 @@ mod tests {
     #[test]
     fn test_serialize_bool() {
         assert_serializes(vec![
-            (true, b"\xff".to_vec()),
-            (false, b"\x00".to_vec()),
+            (true, b"\x01\x01\xff".to_vec()),
+            (false, b"\x01\x01\x00".to_vec()),
         ]);
     }
 
