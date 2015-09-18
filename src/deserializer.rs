ec73057d6cacef7ef6b666a45e01f79f3df3b35a
Alex Gaynor <alex.gaynor@gmail.com>
2015-09-17 22:58:28 -0400

    added all the tests

--- src/deserializer.rs
+++ src/deserializer.rs
@@ -151,7 +151,10 @@ impl Deserializer {
     pub fn read_utctime(&mut self) -> DeserializationResult<DateTime<UTC>> {
         return self._read_with_tag(23, |data| {
             let s = String::from_utf8(data).unwrap();
-            return Ok(UTC.datetime_from_str(&s, "%y%m%d%H%M%SZ").unwrap());
+            return match UTC.datetime_from_str(&s, "%y%m%d%H%M%SZ") {
+                Ok(d) if format!("{}", d.format("%y%m%d%H%M%SZ")) == s => Ok(d),
+                _ => Err(DeserializationError::InvalidValue),
+            }
         });
     }
 
@@ -294,6 +297,32 @@ mod tests {
                 Ok(UTC.timestamp(1258325776, 0)),
                 b"\x17\x0d\x30\x39\x31\x31\x31\x35\x32\x32\x35\x36\x31\x36\x5a".to_vec(),
             ),
+            // TODO: correct hex formatting
+            (Err(DeserializationError::InvalidValue), b"\x17\x0da10506234540Z".to_vec()),
+            (Err(DeserializationError::InvalidValue), b"\x17\x0d91a506234540Z".to_vec()),
+            (Err(DeserializationError::InvalidValue), b"\x17\x0d9105a6234540Z".to_vec()),
+            (Err(DeserializationError::InvalidValue), b"\x17\x0d910506a34540Z".to_vec()),
+            (Err(DeserializationError::InvalidValue), b"\x17\x0d910506334a40Z".to_vec()),
+            (Err(DeserializationError::InvalidValue), b"\x17\x0d91050633444aZ".to_vec()),
+            (Err(DeserializationError::InvalidValue), b"\x17\x0d910506334461Z".to_vec()),
+            (Err(DeserializationError::InvalidValue), b"\x17\x0e910506334400Za".to_vec()),
+            (Err(DeserializationError::InvalidValue), b"\x17\x0d000100000000Z".to_vec()),
+            (Err(DeserializationError::InvalidValue), b"\x17\x0d101302030405Z".to_vec()),
+            (Err(DeserializationError::InvalidValue), b"\x17\x0d100002030405Z".to_vec()),
+            (Err(DeserializationError::InvalidValue), b"\x17\x0d100100030405Z".to_vec()),
+            (Err(DeserializationError::InvalidValue), b"\x17\x0d100132030405Z".to_vec()),
+            (Err(DeserializationError::InvalidValue), b"\x17\x0d100231030405Z".to_vec()),
+            (Err(DeserializationError::InvalidValue), b"\x17\x0d100102240405Z".to_vec()),
+            (Err(DeserializationError::InvalidValue), b"\x17\x0d100102036005Z".to_vec()),
+            (Err(DeserializationError::InvalidValue), b"\x17\x0d100102030460Z".to_vec()),
+            (Err(DeserializationError::InvalidValue), b"\x17\x0e-100102030410Z".to_vec()),
+            (Err(DeserializationError::InvalidValue), b"\x17\x0e10-0102030410Z".to_vec()),
+            (Err(DeserializationError::InvalidValue), b"\x17\x0e10-0002030410Z".to_vec()),
+            (Err(DeserializationError::InvalidValue), b"\x17\x0e1001-02030410Z".to_vec()),
+            (Err(DeserializationError::InvalidValue), b"\x17\x0e100102-030410Z".to_vec()),
+            (Err(DeserializationError::InvalidValue), b"\x17\x0e10010203-0410Z".to_vec()),
+            (Err(DeserializationError::InvalidValue), b"\x17\x0e1001020304-10Z".to_vec()),
+
         ], |deserializer| {
             return deserializer.read_utctime();
         });
