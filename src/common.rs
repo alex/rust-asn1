9466072c9dd4aaf13c55dcfa4e900942922b4c9e
Alex Gaynor <alex.gaynor@gmail.com>
2015-11-09 22:29:07 -0500

    file

---
+++ src/common.rs
@@ -0,0 +1,10 @@
+pub enum ASN1Tag {
+    Bool = 1,
+    Integer = 2,
+    BitString = 3,
+    OctetString = 4,
+    ObjectIdentifier = 6,
+    PrintableString = 19,
+    UTCTime = 23,
+    Sequence = 48,
+}
