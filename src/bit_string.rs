792023498a0e9b5edf92affa043a80f2607c395a
Alex Gaynor <alex.gaynor@gmail.com>
2019-09-02 14:46:29 -0400

    Document BitString

--- src/bit_string.rs
+++ src/bit_string.rs
@@ -1,3 +1,4 @@
+/// Represents an ASN.1 `BIT STRING`.
 #[derive(Debug, PartialEq)]
 pub struct BitString<'a> {
     data: &'a [u8],
@@ -14,5 +15,45 @@ impl<'a> BitString<'a> {
         }
 
         Some(BitString { data, padding_bits })
+    }
+
+    /// Returns a sequence of bytes representing the data in the `BIT STRING`. Padding bits will
+    /// always be 0.
+    pub fn as_bytes(&self) -> &[u8] {
+        self.data
+    }
+
+    /// Returns the number of padding bits. Will always be in [0, 8).
+    pub fn padding_bits(&self) -> u8 {
+        self.padding_bits
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::BitString;
+
+    #[test]
+    fn test_bitstring_new() {
+        assert_eq!(BitString::new(b"abc", 8), None);
+        assert_eq!(BitString::new(b"", 2), None);
+        assert_eq!(BitString::new(b"\xff", 1), None);
+
+        assert!(BitString::new(b"\xff", 0).is_some());
+        assert!(BitString::new(b"\xfe", 1).is_some());
+    }
+
+    #[test]
+    fn test_bitstring_as_bytes() {
+        let bs = BitString::new(b"\xfe", 1).unwrap();
+        assert_eq!(bs.as_bytes(), b"\xfe");
+    }
+
+    #[test]
+    fn test_bitstring_padding_bits() {
+        let bs = BitString::new(b"\xfe", 1).unwrap();
+        assert_eq!(bs.padding_bits(), 1);
+        let bs = BitString::new(b"\xe0", 5).unwrap();
+        assert_eq!(bs.padding_bits(), 5);
     }
 }
