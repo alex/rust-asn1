be8937d7b2d2449251193f9412547970f1e23782
Alex Gaynor <alex.gaynor@gmail.com>
2019-10-20 11:30:31 -0400

    Added a fuzzer for parsing and run it in travis

---
+++ fuzz/fuzz_targets/fuzz_asn1_parse.rs
@@ -0,0 +1,26 @@
+#![no_main]
+#[macro_use]
+extern crate libfuzzer_sys;
+
+libfuzzer_sys::fuzz_target!(|data: &[u8]| {
+    let _ = asn1::parse(data, |d| {
+        d.read_element::<()>()?;
+        d.read_element::<bool>()?;
+
+        d.read_element::<i8>()?;
+        d.read_element::<u8>()?;
+        d.read_element::<i64>()?;
+        d.read_element::<u64>()?;
+
+        d.read_element::<&[u8]>()?;
+        d.read_element::<asn1::PrintableString>()?;
+        d.read_element::<asn1::ObjectIdentifier>()?;
+        d.read_element::<asn1::BitString>()?;
+        d.read_element::<asn1::UtcTime>()?;
+
+        d.read_element::<Option<()>>()?;
+        d.read_element::<asn1::Choice2<bool, i64>>()?;
+
+        Ok(())
+    });
+});
