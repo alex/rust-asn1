9b36a1371d4f2afdc7c894cf4cc7b78d737b087f
Alex Gaynor <alex.gaynor@gmail.com>
2021-09-19 16:09:16 -0400

    Revert "Just write these like OIDs"
    This reverts commit a5ccda3d9bff41ab4b92c3512692bed4a3725426.

--- tests/oid_tests.rs
+++ tests/oid_tests.rs
@@ -3,15 +3,15 @@
 #[test]
 fn test_oid_value() {
     assert_eq!(
-        asn1::oid!(1.2.3.4),
+        asn1::oid!(1, 2, 3, 4),
         asn1::ObjectIdentifier::from_string("1.2.3.4").unwrap()
     );
 }
 
 #[test]
 fn test_match_statement() {
-    const OID1: asn1::ObjectIdentifier = asn1::oid!(1.2.3.4);
-    const OID2: asn1::ObjectIdentifier = asn1::oid!(1.2.3.5);
+    const OID1: asn1::ObjectIdentifier = asn1::oid!(1, 2, 3, 4);
+    const OID2: asn1::ObjectIdentifier = asn1::oid!(1, 2, 3, 5);
 
     let oid = asn1::ObjectIdentifier::from_string("1.2.3.4").unwrap();
     let result = match oid {
