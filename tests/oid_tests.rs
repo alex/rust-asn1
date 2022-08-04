0772931ed3212887f58c2ef3ed6c990f2bb764b4
Alex Gaynor <alex.gaynor@gmail.com>
2022-08-04 07:58:41 -0400

    refs #274 -- complete coverage for tests/

--- tests/oid_tests.rs
+++ tests/oid_tests.rs
@@ -14,10 +14,6 @@ fn test_match_statement() {
     const OID2: asn1::ObjectIdentifier = asn1::oid!(1, 2, 3, 5);
 
     let oid = asn1::ObjectIdentifier::from_string("1.2.3.4").unwrap();
-    let result = match oid {
-        OID1 => "abc",
-        OID2 => "def",
-        _ => "ghi",
-    };
-    assert_eq!(result, "abc");
+    assert!(matches!(oid, OID1));
+    assert!(!matches!(oid, OID2));
 }
