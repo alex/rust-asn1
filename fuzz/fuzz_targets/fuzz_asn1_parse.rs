b6c8e5a58e0bce6a22f6a9bc3cc2a68f56e93f18
Alex Gaynor <alex.gaynor@gmail.com>
2022-08-30 18:11:16 -0400

    Added more types to fuzzer (#316)

--- fuzz/fuzz_targets/fuzz_asn1_parse.rs
+++ fuzz/fuzz_targets/fuzz_asn1_parse.rs
@@ -18,18 +18,43 @@ struct Data<'a> {
     f10: asn1::PrintableString<'a>,
     f11: asn1::BMPString<'a>,
     f12: asn1::UniversalString<'a>,
-    f13: asn1::BitString<'a>,
+    f13: asn1::IA5String<'a>,
+    f14: asn1::Utf8String<'a>,
+    f15: asn1::VisibleString<'a>,
 
-    f14: asn1::ObjectIdentifier,
+    f16: asn1::BitString<'a>,
+    f17: asn1::OwnedBitString,
 
-    f15: asn1::UtcTime,
-    f16: asn1::GeneralizedTime,
+    f18: asn1::ObjectIdentifier,
 
-    f17: Option<()>,
-    f18: asn1::Choice2<bool, i64>,
+    f19: asn1::UtcTime,
+    f20: asn1::GeneralizedTime,
 
-    f19: asn1::SequenceOf<'a, i64>,
-    f20: asn1::SetOf<'a, i64>,
+    f21: asn1::Enumerated,
+
+    f22: Option<()>,
+    f23: asn1::Choice2<bool, i64>,
+
+    f24: asn1::Sequence<'a>,
+    f25: asn1::SequenceOf<'a, i64>,
+    f26: asn1::SetOf<'a, i64>,
+
+    #[implicit(3)]
+    f27: Option<u32>,
+    #[implicit(4)]
+    f28: Option<u32>,
+
+    #[implicit(3)]
+    #[default(7)]
+    f29: i32,
+    #[implicit(4)]
+    #[default(8)]
+    f30: i32,
+
+    #[cfg(feature = "const-generics")]
+    f31: asn1::Implicit<u32, 3>,
+    #[cfg(feature = "const-generics")]
+    f32: asn1::Explicit<u32, 3>,
 }
 
 fuzz_target!(|data: &[u8]| {
