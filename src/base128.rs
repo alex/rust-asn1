efc54e36f64bd8e286192931be2f894a48c386cb
Alex Gaynor <alex.gaynor@gmail.com>
2023-08-09 16:35:52 -0400

    Fixed handling of large values in base128 (#409)

--- src/base128.rs
+++ src/base128.rs
@@ -2,12 +2,15 @@ use crate::parser::{ParseError, ParseErrorKind, ParseResult};
 
 pub(crate) fn read_base128_int(mut data: &[u8]) -> ParseResult<(u32, &[u8])> {
     let mut ret = 0u32;
-    for i in 0..4 {
+    for i in 0..5 {
         let b = match data.first() {
             Some(b) => *b,
             None => return Err(ParseError::new(ParseErrorKind::InvalidValue)),
         };
         data = &data[1..];
+        if ret > u32::MAX >> 7 {
+            return Err(ParseError::new(ParseErrorKind::InvalidValue));
+        }
         ret <<= 7;
         ret |= u32::from(b & 0x7f);
         // Integers must be minimally encoded. `i == 0 && 0x80` would mean
@@ -58,4 +61,27 @@ pub(crate) fn write_base128_int(mut data: &mut [u8], n: u32) -> Option<usize> {
     }
 
     Some(length)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::{read_base128_int, write_base128_int};
+
+    #[test]
+    fn test_read_overflow() {
+        let buf = [0x90, 0x80, 0x80, 0x80, 0x0];
+        let result = read_base128_int(&buf);
+        assert!(result.is_err());
+    }
+
+    #[test]
+    fn test_roundtrip() {
+        for i in [0, 10, u32::MAX] {
+            let mut buf = [0; 16];
+            let length = write_base128_int(&mut buf, i).unwrap();
+            let (val, remainder) = read_base128_int(&buf[..length]).unwrap();
+            assert_eq!(i, val);
+            assert!(remainder.is_empty());
+        }
+    }
 }
